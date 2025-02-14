d4a6ec017c43d55476fe631c6b4024207556a682
giok94 <gkakulashvili@digitech.marketing>
2025-02-14 11:57:45 +0400

    making notes

--- mathai/activations.py
+++ mathai/activations.py
@@ -1,35 +1,53 @@
 import numpy as np
 
 
-def line(x, diff=False):
-    if diff:
-        return 1
-    return x
+class TanH:
+
+    def calc(self, x):
+        return np.tanh(x)
+
+    def dcalc(self, x):
+        return 1 / np.cosh(x) ** 2
 
 
-def leaky_relu(x, alpha=0.01, diff=False):
+class ArcTan:
 
-    if x >= 0:
-        return x if diff else 1
+    def calc(self, x):
+        return np.atan(x)
 
-    return alpha * x if diff else alpha
-
-
-def elu(x, alpha=0.01, diff=False):
-
-    if x >= 0:
-        return x if diff else 1
-
-    return alpha * (np.exp(x) - 1) if diff else alpha * np.exp(x)
-
-
-def sigmoid(x, derivitive=False):
-    if derivitive:
-        return sigmoid(x) * (1 - sigmoid(x))
-    return 1 / (1 + np.exp(-x))
-
-
-def atan(x, derivitive=False):
-    if derivitive:
+    def dcalc(self, x):
         return 1 / (1 + x**2)
-    return np.atan(x)
+
+
+# def line(x, diff=False):
+#     if diff:
+#         return 1
+#     return x
+
+
+# def leaky_relu(x, alpha=0.01, diff=False):
+
+#     if x >= 0:
+#         return x if diff else 1
+
+#     return alpha * x if diff else alpha
+
+
+# def elu(x, alpha=0.01, diff=False):
+
+#     if x >= 0:
+#         return x if diff else 1
+
+#     return alpha * (np.exp(x) - 1) if diff else alpha * np.exp(x)
+
+
+# def sigmoid(x, derivitive=False):
+#     if derivitive:
+#         return sigmoid(x) * (1 - sigmoid(x))
+#     return 1 / (1 + np.exp(-x))
+
+
+# def atan(x, derivitive=False):
+#     if derivitive:
+#         return 1 / (1 + x**2)
+#     return np.atan(x)
